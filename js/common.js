1cb52e9e867917ab9951faf9db4c5914af518b61
unknown <you@example.com>
2018-05-09 01:45:17 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,738 @@
+$(document).ready(function() {
+	// remove placeholder after click
+	$(document).ready(function () {
+		$('input, textarea').focus(function(){
+			$(this).data('placeholder',$(this).attr('placeholder'))
+			$(this).attr('placeholder','');
+		});
+		$('input, textarea').blur(function(){
+			$(this).attr('placeholder', $(this).data('placeholder'));
+		});
+ 	});
+
+ 	var item = document.querySelectorAll('.address i');
+ 	var map = document.getElementsByClassName('map')[0];
+ 	for(var i = 0; i < item.length; i++){
+ 		item[i].onclick = function(){
+ 			for (var j = 0; j < item.length; j++){
+ 				item[j].classList.remove('active');
+ 			}
+ 			var index = this.dataset.map;
+ 			this.classList.add('active');
+ 			map.setAttribute('src', 'img/map_' + index + '.jpg');
+ 		}
+ 	}
+ 	
+
+	
+
+	function updater(d, h, m, s) {
+	  // День сброса - 5 августа 2017 года (и далее каждые три дня)
+	  var baseTime = new Date(2017, 07, 8);
+	  // Период сброса — 3 дня
+	  var period = 2*24*60*60*1000;
+
+	  function update() {
+	    var cur = new Date();
+	    // сколько осталось миллисекунд
+	    var diff = period - (cur - baseTime) % period;
+	    // сколько миллисекунд до конца секунды
+	    var millis = diff % 1000;
+	    diff = Math.floor(diff/1000);
+	    // сколько секунд до конца минуты
+	    var sec = diff % 60;
+	    if(sec < 10) sec = "0"+sec;
+	    diff = Math.floor(diff/60);
+	    // сколько минут до конца часа
+	    var min = diff % 60;
+	    if(min < 10) min = "0"+min;
+	    diff = Math.floor(diff/60);
+	    // сколько часов до конца дня
+	    var hours = diff % 24;
+	    if(hours < 10) hours = "0"+hours;
+	    var days = Math.floor(diff / 24);
+	    d.innerHTML = days;
+	    h.innerHTML = hours;
+	    m.innerHTML = min;
+	    s.innerHTML = sec;
+	  
+	    // следующий раз вызываем себя, когда закончится текущая секунда
+	    setTimeout(update, millis);
+	  }
+	  setTimeout(update, 0);
+	}
+	updater(document.getElementById("days"),
+document.getElementById("hours"), document.getElementById("minutes"),
+document.getElementById("seconds"));
+
+
+
+	
+
+	$('.scroll').click( function(){
+	var scrollEl = $(this).attr('href');
+		if ($(scrollEl).length != 0) {
+			$('html, body').animate({ scrollTop: $(scrollEl).offset().top }, 800);
+		}
+		return false;
+	});
+
+	//popup
+	$('.popup').magnificPopup({
+		type: 'inline'
+	});
+	
+	// photo magnific gallery
+	$('.photo_popup').magnificPopup({
+		type: 'image',
+		gallery: {
+			enabled: true
+		}
+	});
+
+	//mask
+	jQuery(function($){
+		$('.phone').mask('+38(099) 999-9999');
+	});
+
+	$('.photo img').on('click', function(e){
+		$('.photo img').parent().removeClass('active');
+		$(this).parent().addClass('active');
+	});
+
+
+	/*
+	//slick carousel
+	$('.company_slider').slick({
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		nextArrow: '.company_next',
+		prevArrow: '.company_prev',
+		asNavFor: '.photo_carousel_for',
+		focusOnSelect: true,
+		appendDots: '.review .nav_slider',
+		customPaging: function(slider, i) {
+			return '<div class="dot"></div>';
+		},
+		responsive: [
+			{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3
+			}
+			}
+		]
+	});
+	*/
+	//slick carousel
+	$('.doctor_slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		nextArrow: '.doctor_next',
+		prevArrow: '.doctor_prev',
+		responsive: [
+			{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2
+			}
+			},
+			{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 1
+			}
+			}
+		]
+	});
+
+	$('.cert_slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		nextArrow: '.cert_next',
+		prevArrow: '.cert_prev',
+		dots: true,
+		appendDots: '.cert h3',
+		customPaging: function(slider, i) {
+			return '<div class="dot"></div>';
+		},
+		responsive: [
+			{
+			breakpoint: 1200,
+			settings: {
+				dots: false
+			}
+			},
+			{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2,
+				dots: false
+			}
+			}
+		]
+	});
+
+	
+	//video player + button
+	$('#mainVideo').click(function(){
+		if (this.paused) {
+			this.play();
+			$('#videoButton').fadeOut('fast');
+		} else {
+			$('#videoButton').fadeIn('fast');
+			this.pause();
+		}
+	});
+	$('.play').on('click', function(event) {
+		event.preventDefault();
+		$('#videoButton').fadeOut('fast');
+		$('#mainVideo').get(0).play();
+	});
+
+	
+	//validation
+	var locationURL = window.location.pathname;
+	if ( locationURL == "/ua" ) {
+		var validationName = "Обов'язково для заповнення";
+		var validationNameMax = "Від 2 до 16 літер";
+		var validationPhone = "Введіть вірний номер";
+		var validationEmail = "Введіть вірний E-mail";
+	}
+	else if ( locationURL == "/en" ) {
+		var validationName = "Fill in this field";
+		var validationNameMax = "From 2 till 16 letters";
+		var validationPhone = "Fill in the correct phone";
+		var validationEmail = "Fill in the correct email";
+	}
+	else {
+		var validationName = "Обязательно для заполнения";
+		var validationNameMax = "От 2 до 16 букв";
+		var validationPhone = "Введите корректный номер";
+		var validationEmail = "Введите корректный E-mail";
+	}
+
+
+	$('#topForm').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: validationName,
+				minlength: validationNameMax,
+				maxlength: validationNameMax
+			},
+			email: {
+				required: validationName,
+				email: validationEmail
+			},
+			phone: {
+				required: validationPhone
+			}
+		}
+	});
+	$('#mainForm').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: validationName,
+				minlength: validationNameMax,
+				maxlength: validationNameMax
+			},
+			email: {
+				required: validationName,
+				email: validationEmail
+			},
+			phone: {
+				required: validationPhone
+			}
+		}
+	});
+
+	$('#service1Form').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: validationName,
+				minlength: validationNameMax,
+				maxlength: validationNameMax
+			},
+			email: {
+				required: validationName,
+				email: validationEmail
+			},
+			phone: {
+				required: validationPhone
+			}
+		}
+	});
+	$('#service2Form').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: validationName,
+				minlength: validationNameMax,
+				maxlength: validationNameMax
+			},
+			email: {
+				required: validationName,
+				email: validationEmail
+			},
+			phone: {
+				required: validationPhone
+			}
+		}
+	});
+	$('#service3Form').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: validationName,
+				minlength: validationNameMax,
+				maxlength: validationNameMax
+			},
+			email: {
+				required: validationName,
+				email: validationEmail
+			},
+			phone: {
+				required: validationPhone
+			}
+		}
+	});
+	$('#service4Form').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: validationName,
+				minlength: validationNameMax,
+				maxlength: validationNameMax
+			},
+			email: {
+				required: validationName,
+				email: validationEmail
+			},
+			phone: {
+				required: validationPhone
+			}
+		}
+	});
+	$('#service5Form').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: validationName,
+				minlength: validationNameMax,
+				maxlength: validationNameMax
+			},
+			email: {
+				required: validationName,
+				email: validationEmail
+			},
+			phone: {
+				required: validationPhone
+			}
+		}
+	});
+	$('#service6Form').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: validationName,
+				minlength: validationNameMax,
+				maxlength: validationNameMax
+			},
+			email: {
+				required: validationName,
+				email: validationEmail
+			},
+			phone: {
+				required: validationPhone
+			}
+		}
+	});
+	$('#bottom_Form').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: validationName,
+				minlength: validationNameMax,
+				maxlength: validationNameMax
+			},
+			email: {
+				required: validationName,
+				email: validationEmail
+			},
+			phone: {
+				required: validationPhone
+			}
+		}
+	});
+	$('#contactForm').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: validationName,
+				minlength: validationNameMax,
+				maxlength: validationNameMax
+			},
+			email: {
+				required: validationName,
+				email: validationEmail
+			},
+			phone: {
+				required: validationPhone
+			}
+		}
+	});
+	$('#doctor1Form').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: validationName,
+				minlength: validationNameMax,
+				maxlength: validationNameMax
+			},
+			email: {
+				required: validationName,
+				email: validationEmail
+			},
+			phone: {
+				required: validationPhone
+			}
+		}
+	});
+	$('#doctor2Form').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: validationName,
+				minlength: validationNameMax,
+				maxlength: validationNameMax
+			},
+			email: {
+				required: validationName,
+				email: validationEmail
+			},
+			phone: {
+				required: validationPhone
+			}
+		}
+	});
+	$('#doctor3Form').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: validationName,
+				minlength: validationNameMax,
+				maxlength: validationNameMax
+			},
+			email: {
+				required: validationName,
+				email: validationEmail
+			},
+			phone: {
+				required: validationPhone
+			}
+		}
+	});
+	$('#doctor4Form').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: validationName,
+				minlength: validationNameMax,
+				maxlength: validationNameMax
+			},
+			email: {
+				required: validationName,
+				email: validationEmail
+			},
+			phone: {
+				required: validationPhone
+			}
+		}
+	});
+	$('#doctor5Form').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: validationName,
+				minlength: validationNameMax,
+				maxlength: validationNameMax
+			},
+			email: {
+				required: validationName,
+				email: validationEmail
+			},
+			phone: {
+				required: validationPhone
+			}
+		}
+	});
+	$('#doctor6Form').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: validationName,
+				minlength: validationNameMax,
+				maxlength: validationNameMax
+			},
+			email: {
+				required: validationName,
+				email: validationEmail
+			},
+			phone: {
+				required: validationPhone
+			}
+		}
+	});
+	$('#doctor7Form').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: validationName,
+				minlength: validationNameMax,
+				maxlength: validationNameMax
+			},
+			email: {
+				required: validationName,
+				email: validationEmail
+			},
+			phone: {
+				required: validationPhone
+			}
+		}
+	});
+
+	
+
+
+});
+/*
+// preloader
+$(window).load(function(){
+	$('.preloader_inner').fadeOut();
+	$('.preloader').delay(100).fadeOut('fast');
+});
+*/
